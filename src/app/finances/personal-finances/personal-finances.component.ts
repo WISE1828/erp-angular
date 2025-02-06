0b2d944cb01d8536c2235c61ba52cd40a9160e4c
Pavel <workmepaul@gmail.com>
2025-02-06 12:40:28 +0300

    rename slices, fix ResultMoney

--- src/app/finances/personal-finances/personal-finances.component.ts
+++ src/app/finances/personal-finances/personal-finances.component.ts
@@ -593,19 +593,34 @@ export class PersonalFinancesComponent implements OnInit {
     //return checkNumber(this.income - this.expose, 0);
   }
 
+  // get getResultMoneyOld(): number {
+  //   let result = 0;
+  //   let percent = 0;
+  //   this.currentItems.forEach(el => {
+  //     const comissionTax = checkNumber(el.comissionTax + el.comissionTaxUsd * el.usdRub, 0);
+  //     const profit = checkNumber(el.profit, 0);
+  //     const accountsTax = checkNumber(el.accountsTax + el.accountsTaxUsd * el.usdRub, 0);
+  //     const clearProfit = checkNumber(el.isInternship ? 0 : profit - comissionTax - accountsTax, 0);
+  //     result += (clearProfit * el.profitPercent) / 100;
+  //     percent += el.profitPercent;
+  //   });
+  //   percent = percent / this.currentItems.length / 100;
+  //   return checkNumber(result + this.refundsToRub * percent, 0);
+  // }
+
   get getResultMoney(): number {
     let result = 0;
     let percent = 0;
     this.currentItems.forEach(el => {
-      const comissionTax = checkNumber(el.comissionTax + el.comissionTaxUsd * el.usdRub, 0);
+      const comission = checkNumber(el.commission, 0);
       const profit = checkNumber(el.profit, 0);
-      const accountsTax = checkNumber(el.accountsTax + el.accountsTaxUsd * el.usdRub, 0);
-      const clearProfit = checkNumber(el.isInternship ? 0 : profit - comissionTax - accountsTax, 0);
+      const accountsTax = checkNumber(el.accountsTaxUsd, 0);
+      const clearProfit = checkNumber(el.isInternship ? 0 : profit - comission - accountsTax, 0);
       result += (clearProfit * el.profitPercent) / 100;
       percent += el.profitPercent;
     });
     percent = percent / this.currentItems.length / 100;
-    return checkNumber(result + this.refundsToRub * percent, 0);
+    return checkNumber(result * percent, 0);
   }
 
   // REFUNDS
